37ad700ea012a5e17a7b4124361ef192e8e0d649
소은 <so-eun@so-eun-ui-MacBookAir.local>
2024-10-15 20:22:59 +0900

    Add GLB file with LFS

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    $(document).ready(function() {
+        $('.control01').on('click', function() {
+        $('.main01').attr('visible', 'true').hide().fadeIn(1000); // 애니메이션 효과 추가
+        });
+    });
+
